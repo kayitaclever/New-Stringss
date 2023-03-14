13b9c47e6b30e801a0271b925e6609b4b97ad761
kayitaclever <kayitaramirwaaclever@gmail.com>
2023-03-14 11:46:58 +0300

    first commit

---
+++ Stringss.js
@@ -0,0 +1,37 @@
+const message = "extravaganza"
+let results= message.substring(8)
+console.log(results)
+
+let activity = "eat";
+console.log("The"+" "+activity+" "+"quick fox jumped over the lazy dog")
+
+
+const story="The quick brown jumps over the lazy dog"
+console.count("The")
+console.count("brown")
+
+const string1="The pupils are reading in the libray";
+let position= string1.search("are");
+console.log(position)
+
+const string2="The child was sitting on the table before it fell";
+let position2 = string2.search("sitting");
+console.log(position)
+
+const word1 = "wonderful"
+let results1=word1.toUpperCase()
+console.log(results1)
+
+const word2="amazing"
+let results2=word2.toLowerCase()
+console.log(results2)
+
+const word3="undERneath"
+let results3= word3.toLowerCase()
+console.log(results3)
+
+const noun="A wonderful world"
+var sentence= noun;{
+    sentence[2].toUpperCase,sentence[12].toString
+}
+console.log(sentence)
